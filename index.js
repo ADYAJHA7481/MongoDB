c0535e650b4364d59ae454d93f9ea0c2180c9699
Aditya Kashyap <145245185+ADYAJHA7481@users.noreply.github.com>
2024-08-28 19:36:12 +0530

    Insert Data in Database

--- index.js
+++ index.js
@@ -6,6 +6,8 @@ const Chat = require("./models/chat.js");
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
+app.use(express.static(path.join(__dirname,"public")));
+app.use(express.urlencoded({extended: true}))
 
 main().
 then(()=>{
@@ -18,16 +20,38 @@ async function main() {
     await mongoose.connect("mongodb://127.0.0.1/whatsapp");
 }
 
-let chat1 = new Chat({
-    from : "neha",
-    to : "priya",
-    msg:"send me your exam sheet",
-    created_at: new Date(),
+//Index Route
+app.get("/chats", async (req,res)=> {
+    let chats = await Chat.find();
+    console.log("Succesful");
+    res.render("index.ejs",{chats});
 })
 
-chat1.save().then((res)=>{
-    console.log(res);
-});
+//New Route
+app.get("/chats/new",(req,res)=>{
+    res.render("new.ejs");
+})
+
+//Create Route
+app.post("/chats",(req,res)=>{
+    let {from,to,msg} = req.body;
+    let newChat = new Chat({
+        from : from,
+        to : to,
+        msg : msg,
+        created_at: new Date()
+    });
+    newChat
+    .save()
+    .then(res=>{
+        console.log("Chat was saved!!!")
+    })
+    .catch((err)=>{
+        console.log(err);
+    });
+    res.redirect("/chats");
+})
+
 
 app.get("/",(req,res) =>{
     res.send("root is working");
---
+++ init.js
@@ -0,0 +1,36 @@
+const mongoose = require("mongoose");
+const Chat = require("./models/chat.js");
+
+main().
+then(()=>{
+    console.log("connection successful")})
+.catch(err=>{
+    console.log(err);
+});
+async function main() {
+    await mongoose.connect("mongodb://127.0.0.1/whatsapp");
+}
+
+let allChat = [
+    {
+    from : "neha",
+    to : "priya",
+    msg:"send me your exam sheet",
+    created_at: new Date(),
+    },
+    {
+        from : "neha",
+        to : "preeti",
+        msg:"send me notes for exam ",
+        created_at: new Date(),
+    },
+    {
+        from : "tanya",
+        to : "priya",
+        msg:"send me your pic",
+        created_at: new Date(),
+    },  
+
+]
+
+Chat.insertMany(allChat);
